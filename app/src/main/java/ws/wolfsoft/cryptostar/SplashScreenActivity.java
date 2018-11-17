b07989e12c7c0527b94b6c528cfa2927e490c07b
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-17 10:05:30 +0530

    Initial commit

---
+++ app/src/main/java/ws/wolfsoft/cryptostar/SplashScreenActivity.java
@@ -0,0 +1,51 @@
+package ws.wolfsoft.cryptostar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(2*5000);
+
+                    Intent intent = new Intent(SplashScreenActivity.this,CryptoListActivity.class);
+                    startActivity(intent);
+
+
+                    // After 5 seconds redirect to another intent
+
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+    }
+
+
+}
